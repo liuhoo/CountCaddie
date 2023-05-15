99ff35ef0da0fce4be2e3fbfbdf52fd603190dfe
Alex Liu <alexliulxl@gmail.com>
2023-05-14 18:29:00 -0700

    Initial Commit

---
+++ CountCaddie/CountCaddieApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountCaddieApp.swift
+//  CountCaddie
+//
+//  Created by Alex Liu on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct CountCaddieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

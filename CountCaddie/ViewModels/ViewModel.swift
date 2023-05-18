bb18dd647b9bccf77947ad3a26139466bb108b27
Alex Liu <alexliulxl@gmail.com>
2023-05-17 23:38:41 -0700

    Initial hole navigation implementation.

--- CountCaddie/ViewModels/ViewModel.swift
+++ CountCaddie/ViewModels/ViewModel.swift
@@ -12,9 +12,9 @@ class ViewModel: ObservableObject{
     
     @Published var roundList: [ScorecardModel] =
     [ScorecardModel(id: "BHCC", description: "Test Round", holes: [ScorecardModel.HoleModel(id: 1, FairwayHit: true, GreenHit: "True", Putts: [ScorecardModel.HoleModel.Putt(id:1, Break: "LR", Miss: "F", Make: true)], Score: 4, Par: 4),
-                                                                   ScorecardModel.HoleModel(id: 2,FairwayHit: false, GreenHit: "True", Putts: [ScorecardModel.HoleModel.Putt(id:1, Break: "RL", Miss: "F", Make: false), ScorecardModel.HoleModel.Putt(id:1, Break: "LR", Miss: "F", Make: true)], Score: 4, Par: 4),
-                                                                   ScorecardModel.HoleModel(id: 3,FairwayHit: true, GreenHit: "SHEEEEESH", Putts: [ScorecardModel.HoleModel.Putt(id:1, Break: "LR", Miss: "F", Make: true)], Score: 4, Par: 4),
-                                                                   ScorecardModel.HoleModel(id: 4,FairwayHit: true, GreenHit: "True", Putts: [ScorecardModel.HoleModel.Putt(id:1, Break: "RL", Miss: "F", Make: false), ScorecardModel.HoleModel.Putt(id:1, Break: "LR", Miss: "F", Make: true)], Score: 4, Par: 4)])]
+   ScorecardModel.HoleModel(id: 2,FairwayHit: false, GreenHit: "True", Putts: [ScorecardModel.HoleModel.Putt(id:1, Break: "RL", Miss: "F", Make: false), ScorecardModel.HoleModel.Putt(id:1, Break: "LR", Miss: "F", Make: true)], Score: 4, Par: 4),
+   ScorecardModel.HoleModel(id: 3,FairwayHit: true, GreenHit: "SHEEEEESH", Putts: [ScorecardModel.HoleModel.Putt(id:1, Break: "LR", Miss: "F", Make: true)], Score: 4, Par: 4),
+   ScorecardModel.HoleModel(id: 4,FairwayHit: true, GreenHit: "True", Putts: [ScorecardModel.HoleModel.Putt(id:1, Break: "RL", Miss: "F", Make: false), ScorecardModel.HoleModel.Putt(id:1, Break: "LR", Miss: "F", Make: true)], Score: 4, Par: 4)])]
     func addRound(RoundName: String, RoundDescription: String){
         let newItem = ScorecardModel(id: RoundName,  description: RoundDescription)
         roundList.append(newItem)
--- CountCaddie/Views/CollectDataView.swift
+++ CountCaddie/Views/CollectDataView.swift
@@ -16,14 +16,26 @@ struct CollectDataView: View {
     @StateObject private var vm = ViewModel()
     
     var body: some View {
-        VStack{
-            Text("Hole Number")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
+        
+        VStack(alignment: .center, spacing:0){
+            
+            ZStack{
+                
+                LazyVGrid(columns: [GridItem(),GridItem(),GridItem()]){
+                    ForEach(1..<19) {i in
+                        NavigationLink(destination: Text("\(i)")){HoleSelectView(HoleNumber: i)}.navigationTitle("Hole Number")
+                    }
+                }.padding().cornerRadius(20).overlay(RoundedRectangle(cornerRadius: 10).inset(by: -10).strokeBorder(lineWidth: 1 ).padding(.all))
+                
+                
+            }.frame(maxHeight: UIScreen.main.bounds.size.height/4)
+            
+            
             List{
                 HStack{
                     Stepper("Par: \(ParStepper)", value: $ParStepper)
                 }
+                
                 HStack{
                     Stepper("Score: \(ScoreStepper)", value: $ScoreStepper)
                 }
@@ -41,22 +53,22 @@ struct CollectDataView: View {
                     Button{}label: {  Image(systemName: "checkmark.square")}
                     
                 }.font(.title2)
-//                ForEach($vm.holes) {hole in
-//                    HStack{
-//                        Text("Putt").font(.body)
-//                        Spacer()
-//                        Button{}label: {  Image(systemName: "arrow.uturn.left.circle")}
-//                        Button{}label: {  Image(systemName: "arrow.up.circle")}
-//                        Button{}label: {  Image(systemName: "arrow.uturn.right.circle")}
-//
-//                        Button{}label: {  Image(systemName: "pause")}
-//
-//                        Button{}label: {  Image(systemName: "arrow.up.left.circle")}
-//                        Button{}label: {  Image(systemName: "arrow.up.right.circle")}
-//                        Button{}label: {  Image(systemName: "arrow.up.circle")}
-//                        Button{}label: {  Image(systemName: "arrow.down.circle")}
-//                    }.font(.title3)
-//                }
+                ForEach(vm.getHole(index: 0, holeNo: 1).Putts) {putt in
+                    HStack{
+                        Text("Putt").font(.body)
+                        Spacer()
+                        Button{}label: {  Image(systemName: "arrow.uturn.left.circle")}
+                        Button{}label: {  Image(systemName: "arrow.up.circle")}
+                        Button{}label: {  Image(systemName: "arrow.uturn.right.circle")}
+
+                        Button{}label: {  Image(systemName: "pause")}
+
+                        Button{}label: {  Image(systemName: "arrow.up.left.circle")}
+                        Button{}label: {  Image(systemName: "arrow.up.right.circle")}
+                        Button{}label: {  Image(systemName: "arrow.up.circle")}
+                        Button{}label: {  Image(systemName: "arrow.down.circle")}
+                    }.font(.title3)
+                }
                 HStack{
                     Text("Putt").font(.body)
                     Spacer()
@@ -76,9 +88,23 @@ struct CollectDataView: View {
                     Button{}label: {  HStack{Text("Add Putt");Image(systemName: "plus.rectangle")}}
                     Spacer()
                 }.font(.title2)
+        
+                
             }
             .padding()
         }
+    }
+}
+
+
+struct HoleSelectView: View {
+    var HoleNumber: Int
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 10).stroke()
+            Text("\(HoleNumber)").font(.body)
+        }.padding(.horizontal)
+        
     }
 }
 
